dc71778b200a7c91802f17556b1dfdbb4ab5679d
Stiben Ramos <aldair.ramos@udea.edu.co>
2024-10-15 18:47:13 -0500

    Se corrigieron errores en el codigo

--- main.cpp
+++ main.cpp
@@ -1,31 +1,8 @@
 #include <iostream>
 #include <string>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
-
-class Tanque{
-private:
-    int id_tanque;
-    int capacidad;
-public:
-    Tanque();
-    ~Tanque();
-};
-
-class Venta{
-private:
-    int nro;
-    string fecha;
-    string hora;
-    int cantidad;
-    string tipo_combustible;
-    string metodo_pago;
-    int nro_doc_cliente;
-    int valor;
-public:
-    Venta();
-    void registrarVenta();
-    ~Venta();
-};
 
 class Surtidor{
 private:
@@ -38,7 +15,7 @@ public:
     int obtenerId() const {
         return id;
     }
-    void vender(float cantidad);
+    void vender(float cantidad, string region, const string &precio_combustible);
     float obtenerVentas() const { // metodo para total de ventas
         return ventas; // Total de ventas
     }
@@ -47,7 +24,7 @@ public:
     }
 };
 
-class Estacion {
+class Estacion{
 private:
     long id_estacion;
     string gerente;
@@ -79,6 +56,7 @@ public:
     void eliminarSurtidor(int id);
     void activarSurtidor(int id );
     void desactivarSurtidor(int id );
+    static string cambiar_precios(string region, const string &precio_combustible);
     // consultarHistorico();
     //asignarCapacidad();
     //verificarFugas();
@@ -92,18 +70,48 @@ private:
     int totalEstaciones;
     string nombre_red;
 public:
-    Red(string _nombre_red, int _precio_combustible);
-    ~Red();
+    int getTotalEstaciones(){
+        return totalEstaciones;
+    }
+    Red(string _nombre_red, string _precio_combustible);
     void agregarEstacion();
     void eliminarEstacion(long codigo); // Metodo para eliminar una estacion por su codigo
     void calcularVentas();
     void fijarPrecios();
     // Metodo para mostrar informacion de la red y sus estaciones
     void mostrarEstaciones() const;
+    ~Red();
+};
+
+
+class Tanque{
+private:
+    int capacidad[3];// Para Regular, Premium, EcoExtra
+    int vendidos [3]; // Para rastrear lo vendido por tipo
+public:
+    Tanque();
+    bool verificarFugas();
+    void vender(int tipo, int cantidad);
+};
+
+class Venta{
+private:
+    int nro;
+    string fecha;
+    string hora;
+    int cantidad;
+    string tipo_combustible;
+    string metodo_pago;
+    int nro_doc_cliente;
+    int valor;
+public:
+    Venta();
+    void registrarVenta();
+    ~Venta();
 };
 
 int main() {
-    Red miRed("TerMax", 10000);
+    Red miRed("TerMax", "10000:10000:10000");
     cout << "Bienvenidos a la red de Servicios TerMax" << endl;
 
     int op_menu = 1;
@@ -131,9 +139,15 @@ int main() {
             }
             case 2: {
                 long codigo;
-                cout << "Ingrese el código de la estacion a eliminar: ";
-                cin >> codigo;
-                miRed.eliminarEstacion(codigo);
+                if (miRed.getTotalEstaciones() == 0){
+                    cout << "No se encontraron estaciones registradas" << endl;
+                }
+                else{
+                    miRed.mostrarEstaciones();
+                    cout << "Ingrese el codigo de la estacion a eliminar: ";
+                    cin >> codigo;
+                    miRed.eliminarEstacion(codigo);
+                }
                 break;
             }
             case 3:
@@ -186,94 +200,10 @@ int main() {
     return 0;
 }
 
-Surtidor::Surtidor(int _id, string _modelo){
-    id =_id;
-    modelo_maquina = _modelo;
-    ventas = 0.0;
-}
-
-void Surtidor::vender(float cantidad){
-    if (cantidad > 0 ){
-        ventas += cantidad; //Acumula cantidad vendida
-        cout<<  "Venta realizada en el surtidor ID " << id << ". Total vendido: " << ventas << " unidades." << endl;
-    }
-}
-
-Estacion::Estacion(string _nombre, long _id, string _gerente, string _region, string _ubicacion, int _max){
-    nombre_estacion = _nombre;
-    id_estacion = _id;
-    gerente = _gerente;
-    region = _region;
-    ubicacion = _ubicacion;
-    maquinas_espendedoras = 0;
-    contar_surtidores = 0;
-    max_surtidores = _max;
-    surtidores = new Surtidor*[max_surtidores]; // reserva de memoria para los surtidores
-}
-
-void Estacion::agregarSurtidor(string modelo){
-    if (contar_surtidores < max_surtidores){
-        surtidores[contar_surtidores] = new Surtidor(contar_surtidores +1, modelo);
-        contar_surtidores ++;
-        maquinas_espendedoras ++;
-        cout << "Se ha agregado exitosamente un surtidor. Modelo " << modelo << ". Total de surtidores: " << contar_surtidores << endl;
-    }
-}
-
-void Estacion::eliminarSurtidor(int id){
-    for (int i = 0; i < contar_surtidores; i++){
-        if (surtidores[i] -> obtenerId()  == id) {
-            if (surtidores[i]->obtenerVentas() == 0) {
-                delete surtidores[i];
-                for(int j = i; j < contar_surtidores -1; j++){
-                    surtidores [j] = surtidores [j + 1]; // reorganizar el arreglo
-                }
-                contar_surtidores --;
-                maquinas_espendedoras --;
-                cout << "El surtidor con ID " << id << " fue eliminado. El total de surtidores actual es " << contar_surtidores << endl;
-                return;
-            }
-        }
-        cout << "Surtidor con ID " << id << " no encontrado." << endl;}
-}
-
-void Estacion::activarSurtidor(int id){
-    for (int i = 0; i < contar_surtidores; i++) {
-        if (surtidores[i] -> obtenerId() == id) {
-            cout << "Surtidor con ID " << id << " activado." << endl;
-            return;
-        }
-    }
-    cout << "Surtidor con ID " << id << " no encontrado." << endl;
-}
-
-void Estacion::desactivarSurtidor(int id){
-    for (int i = 0; i < contar_surtidores; i++) {
-        if ( surtidores[i] -> obtenerId()== id) {
-            cout << "Surtidor con ID " << id << " desactivado." << endl;
-            return;
-        }
-    }
-    cout << "Surtidor con ID " << id << " no encontrado." << endl;
-}
-
-Estacion::~Estacion(){
-    for (int i = 0; i < contar_surtidores; i++){
-        delete surtidores[i]; // libera la memoria de los surtidores
-    }
-    delete[] surtidores;
-}
-
-Red::Red(string _nombre_red, int _precio_combustible) {  // Inicializo el constructor
+Red::Red(string _nombre_red, string _precio_combustible) {  // Inicializo el constructor
     totalEstaciones = 0;
     precio_combustible = _precio_combustible;
     nombre_red = _nombre_red;
-}
-
-Red::~Red() {  // Destructor libera memoria dinamica
-    for (int i = 0; i < totalEstaciones; i++) {
-        delete estaciones[i];
-    }
 }
 
 void Red::agregarEstacion() {
@@ -357,11 +287,165 @@ void Red::fijarPrecios(){
 }
 
 void Red::mostrarEstaciones() const {
-    cout << "Red: " << nombre_red << endl;
+    cout << endl << "Red: " << nombre_red << endl;
     for (int i = 0; i < totalEstaciones; i++){
         cout << "Estacion " << i + 1 << ": " << estaciones[i]->getNombreEstacion()
         << ", Codigo: " << estaciones[i]->getid_Estacion()
         << ", Gerente: " << estaciones[i]->getGerente()
         << ", Region: " << estaciones[i]->getRegion() << endl;
+    }
+}
+
+Red::~Red() {  // Destructor libera memoria dinamica
+    for (int i = 0; i < totalEstaciones; i++) {
+        delete estaciones[i];
+    }
+}
+
+Estacion::Estacion(string _nombre, long _id, string _gerente, string _region, string _ubicacion, int _max){
+    nombre_estacion = _nombre;
+    id_estacion = _id;
+    gerente = _gerente;
+    region = _region;
+    ubicacion = _ubicacion;
+    maquinas_espendedoras = 0;
+    contar_surtidores = 0;
+    max_surtidores = _max;
+    surtidores = new Surtidor*[max_surtidores]; // reserva de memoria para los surtidores
+}
+
+void Estacion::agregarSurtidor(string modelo){
+    if (contar_surtidores < max_surtidores){
+        surtidores[contar_surtidores] = new Surtidor(contar_surtidores +1, modelo);
+        contar_surtidores ++;
+        maquinas_espendedoras ++;
+        cout << "Se ha agregado exitosamente un surtidor. Modelo " << modelo << ". Total de surtidores: " << contar_surtidores << endl;
+    }
+}
+
+void Estacion::eliminarSurtidor(int id){
+    for (int i = 0; i < contar_surtidores; i++){
+        if (surtidores[i] -> obtenerId()  == id) {
+            if (surtidores[i]->obtenerVentas() == 0) {
+                delete surtidores[i];
+                for(int j = i; j < contar_surtidores -1; j++){
+                    surtidores [j] = surtidores [j + 1]; // reorganizar el arreglo
+                }
+                contar_surtidores --;
+                maquinas_espendedoras --;
+                cout << "El surtidor con ID " << id << " fue eliminado. El total de surtidores actual es " << contar_surtidores << endl;
+                return;
+            }
+        }
+        cout << "Surtidor con ID " << id << " no encontrado." << endl;}
+}
+
+void Estacion::activarSurtidor(int id){
+    for (int i = 0; i < contar_surtidores; i++) {
+        if (surtidores[i] -> obtenerId() == id) {
+            cout << "Surtidor con ID " << id << " activado." << endl;
+            return;
+        }
+    }
+    cout << "Surtidor con ID " << id << " no encontrado." << endl;
+}
+
+void Estacion::desactivarSurtidor(int id){
+    for (int i = 0; i < contar_surtidores; i++) {
+        if ( surtidores[i] -> obtenerId()== id) {
+            cout << "Surtidor con ID " << id << " desactivado." << endl;
+            return;
+        }
+    }
+    cout << "Surtidor con ID " << id << " no encontrado." << endl;
+}
+
+string Estacion::cambiar_precios(string region, const string &precio_combustible){
+    string nuevo_precio;
+    int regular, premium, ecoextra; regular = 0; premium = 0; ecoextra = 0;
+    size_t pos1 = precio_combustible.find(":");
+    if (pos1 != string::npos){
+        regular = stoi(precio_combustible.substr(0, pos1));
+        size_t pos2 = precio_combustible.find(":");
+        if (pos2 != string::npos){
+            premium = stoi(precio_combustible.substr(pos1 +1, pos2 - pos1 -1));
+            ecoextra = stoi(precio_combustible.substr(pos2 +1));
+        }
+    }
+    if (region == "Norte"){
+        regular += 1000;
+        premium += 1000;
+        ecoextra += 1000;
+    }
+    else if (region == "Centro"){
+        regular += 800;
+        premium += 800;
+        ecoextra += 800;
+    }
+    else{
+        regular += 1200;
+        premium += 1200;
+        ecoextra += 1200;
+    }
+    nuevo_precio = to_string(regular) + ":" + to_string(premium) + ":" + to_string(ecoextra);
+    return nuevo_precio;
+}
+
+Estacion::~Estacion(){
+    for (int i = 0; i < contar_surtidores; i++){
+        delete surtidores[i]; // libera la memoria de los surtidores
+    }
+    delete[] surtidores;
+}
+
+Surtidor::Surtidor(int _id, string _modelo){
+    id =_id;
+    modelo_maquina = _modelo;
+    ventas = 0.0;
+}
+
+void Surtidor::vender(float cantidad, string region, const string &precio_combustible){
+    string precios = Estacion::cambiar_precios(region, precio_combustible);
+    int regular, premium, ecoextra; regular = 0; premium = 0; ecoextra = 0;
+    size_t pos1 = precios.find(":");
+    if (pos1 != string::npos){
+        regular = stoi(precios.substr(0, pos1));
+        size_t pos2 = precios.find(":");
+        if (pos2 != string::npos){
+            premium = stoi(precios.substr(pos1 +1, pos2 - pos1 -1));
+            ecoextra = stoi(precios.substr(pos2 +1));
+        }
+    }
+    if (cantidad > 0 ){
+        ventas += cantidad; //Acumula cantidad vendida
+        cout<<  "Venta realizada en el surtidor ID " << id << ". Total vendido: " << ventas << " unidades." << endl;
+    }
+}
+// Método para verificar fugas
+Tanque::Tanque() {
+    srand(static_cast<unsigned int>(time(0))); // Inicializa la semilla
+    for (int i = 0; i < 3; i++) {
+        capacidad[i] = rand() % 101 + 100; // Capacidad aleatoria entre 100 y 200 litros
+        vendidos[i] = 0;
+    }
+}
+
+bool Tanque::verificarFugas() {
+    for (int i = 0; i < 3; i++) {
+        if ((capacidad[i] - vendidos[i]) < (0.05 * capacidad[i])) {
+            cout << "Fuga detectada en categoría " << i << endl;
+            return true;
+        }
+    }
+    return false;
+}
+// Método para vender combustible
+void Tanque::vender(int tipo, int cantidad) {
+    if (capacidad[tipo] >= cantidad) {
+        capacidad[tipo] -= cantidad;
+        vendidos[tipo] += cantidad;
+    } else {
+        vendidos[tipo] += capacidad[tipo]; // Vender lo que hay
+        capacidad[tipo] = 0; // Vaciamos el tanque
     }
 }
